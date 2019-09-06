52d827535d51878a90eca6feab28cae7f10c22c7
JonasLarsson78 <jl.7804@gmail.com>
2019-09-06 10:06:36 +0200

    first commit

---
+++ src/pages/about.js
@@ -0,0 +1,13 @@
+import React from "react"
+import { Link } from "gatsby"
+import Header from "../components/header.js"
+
+
+const Aboute = () => (
+  <>
+  <h1>Om företaget</h1>
+  <Header></Header>
+  </>
+)
+
+export default Aboute;
---
+++ src/pages/jobs.js
@@ -0,0 +1,14 @@
+import React from "react"
+import { Link } from "gatsby"
+import Header from "../components/header.js"
+
+
+
+const Jobs = () => (
+  <>
+  <h1>Jobs</h1>
+    <Header/>
+  </>
+)
+
+export default Jobs;
